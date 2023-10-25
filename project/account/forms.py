d5df23c0e36f2e3e3fc7902074550f8bf0fd6307
Oreoluwa Olowola <oreolowolagmail.com>
2023-10-25 10:10:53 +0100

    update

--- project/account/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class RegistrationForm(forms.Form):
-    username = forms.CharField(max_length=20)
-    email = forms.EmailField()
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
-    password = forms.CharField(widget=forms.PasswordInput)
-
